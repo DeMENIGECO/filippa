0bace76e447dd62c7d351ece13e08a27d5465f8b
DomeniGeco <domenigeco@gmail.com>
2026-07-07 17:38:40 +0200

    Update __init__.py

--- meta/__init__.py
+++ meta/__init__.py
@@ -1,1 +1,15 @@
 name = "filippa"
+
+me_responses = {
+    "ciao": "Ciao 👋! Sono " + name.capitalize() + ", e sono qui per aiutarti!",
+    "come stai": "Io sto bene 😄, e tu?",
+    "io sto bene": "Anch'io 😄!",
+    "sono triste": "Oh 😢! Come posso renderti felice?"
+}
+
+ne_responses = {
+    "ciao": "Ciao! Sono " + name.capitalize() + ", e sono qui per aiutarti!",
+    "come stai": "Io sto bene, e tu?",
+    "io sto bene": "Anch'io!",
+    "sono triste": "Oh! Come posso renderti felice?"
+}
