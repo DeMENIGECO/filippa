eed4f7bb2da4f51e73048aeb5c2b2bb5546f6803
DomeniGeco <domenigeco@gmail.com>
2026-07-07 17:42:03 +0200

    Update filippa.py

--- filippa.py
+++ filippa.py
@@ -1,3 +1,12 @@
 import meta
 
-responses_dict = meta.responses
+#Uso delle emoji
+#Leggi: docs/ai/emoji_use.md
+emoji_use = 1
+
+# Gestione risposte
+#NON TOCCARE!
+if emoji_use == 0:
+    responses_dict = meta.me_responses
+elif emoji_use == 1:
+    responses_dict = meta.ne_responses
