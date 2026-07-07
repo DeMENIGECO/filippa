a283dd25d4580ea23a3b9692b80e0e695b9fcd48
DomeniGeco <domenigeco@gmail.com>
2026-07-07 17:47:04 +0200

    Update filippa.py

--- filippa.py
+++ filippa.py
@@ -11,4 +11,4 @@ if emoji_use == 1:
 elif emoji_use == 0:
     responses_dict = meta.ne_responses
 else:
-    raise InterpreterError("[Filippa] Valore uso emoji invalido: " + str(emoji_use) + "\nLeggi: docs/ai/emoji_use.md")
+    ValueError(f"[Filippa] Valore uso emoji invalido: {emoji_use}\nLeggi: docs/ai/emoji_use.md")
