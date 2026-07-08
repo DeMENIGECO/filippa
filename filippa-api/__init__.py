b4aa8b314c9f9eaadc71e4fb7c649c138c3e0afd
DomeniGeco <domenigeco@gmail.com>
2026-07-08 11:23:46 +0200

    Refactor Bot class initialization and fix comparison
    Fix initialization of the Bot class and correct comparison operators in responses_dict method.

--- filippa-api/__init__.py
+++ filippa-api/__init__.py
@@ -1,13 +1,13 @@
-class Bot:
-    self.name = "filippa"
-    self.emojiuse = 1
-    self.mode = 0
-    
+class Bot:  
     def __init__(self):
         """Inizzializza il bot"""
+        self.name = "filippa"
+        self.emojiuse = 1
+        self.mode = 0
         return None
         
     def responses_dict(self):
+        """Ritorna il dizionario in base all'emoji use adatto"""
         me_responses = {
             "ciao": "Ciao 👋! Sono " + self.name.capitalize() + ", e sono qui per aiutarti!",
             "come stai": "Io sto bene 😄, e tu?",
@@ -20,15 +20,16 @@ class Bot:
             "io sto bene": "Anch'io!",
             "sono triste": "Oh! Come posso renderti felice?"
         }
-        if self.emojiuse = 1:
+        if self.emojiuse == 1:
             return me_responses
-        elif self.emojiuse = 0:
+        elif self.emojiuse == 0:
             return ne_emoji
         else:
             raise ValueError("[FilippaAPI] Uso invalido di emoji: leggi docs/ai/emoji_use.md")
             return None
             
     def ask(self, asked):
+        """Chiede una domanda al bot"""
         if key in responses_dict():
             print(responses_dict[key])
         else:
