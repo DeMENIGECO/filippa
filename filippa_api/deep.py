b54cac3b13dce0a38784741d872227a13b859c2d
DomeniGeco <domenigeco@gmail.com>
2026-07-28 14:08:52 +0200

    Create deep.py

---
+++ filippa_api/deep.py
@@ -0,0 +1,37 @@
+class DeepAgent:  
+    def __init__(self):
+        """Inizzializza il bot"""
+        self.name = "filippa"
+        self.emojiuse = 1
+        return None
+        
+    def responses_dict(self):
+        """Ritorna il dizionario in base all'emoji use adatto"""
+        me_responses = {
+            "ciao": "Ciao 👋! Sono " + self.name.capitalize() + ", e sono qui per aiutarti!",
+            "come stai": "Io sto bene 😄, e tu?",
+            "io sto bene": "Anch'io 😄!",
+            "sono triste": "Oh 😢! Come posso renderti felice?"
+        }
+        ne_responses = {
+            "ciao": "Ciao! Sono " + self.name.capitalize() + ", e sono qui per aiutarti!",
+            "come stai": "Io sto bene, e tu?",
+            "io sto bene": "Anch'io!",
+            "sono triste": "Oh! Come posso renderti felice?"
+        }
+        if self.emojiuse == 1:
+            return me_responses
+        elif self.emojiuse == 0:
+            return ne_responses
+        else:
+            raise ValueError("[FilippaAPI] Uso invalido di emoji: leggi docs/ai/emoji_use.md")
+            return None
+            
+    def ask(self, asked):
+        """Chiede una domanda al bot"""
+        asked = asked.strip().lower()
+        if asked in self.responses_dict():
+            print(self.responses_dict()[asked])
+        else:
+            print("Non so rispondere!")
+        
